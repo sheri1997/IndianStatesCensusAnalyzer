066c9c8d74b138eda56982abe882840263095735
Shreesh Pandey <shvspandey@gmail.com>
2021-12-16 12:15:28 +0530

    Displaying Welcome Message on the master branch

---
+++ IndianStatesCensusAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStatesCensusAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Indian States Census Analyzer Problem");//displaying Welcome message on the master branch.
+        }
+    }
+}
